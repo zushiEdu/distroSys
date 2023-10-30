3ec2db0ffbfe139ef02a62f60eda6079708bacf2
Ethan <hubee8329@wrdsb.ca>
2023-10-30 09:21:05 -0400

    Completed Basic Auth

--- Server/server.js
+++ Server/server.js
@@ -9,35 +9,47 @@ var product = require('./product')
 
 // data
 var config = require("./config.json");
-// var users = require('./users.json');
 
+// log config
 console.log("Config:", config);
 
 try {
-    // create server object on port 720
+    // create server for webApp on port based on config file designation
     http.createServer(function (request, response) {
         // set content type
         response.writeHead(200, { 'Content-Type': 'application/json' });
         if (request.url === '/favicon.ico') {
-
+            // ignore favicon requests
         } else {
             // query url for params
             var query = url.parse(request.url, true).query;
             console.log("URL Query:", query);
+            // change modes based on operation provided
             if (query.operation == "get" || query.operation == "return") {
-                var returnedProduct = product.getProduct(query.sku);
-                // look for sku in database, return object if sku is matching
-                if (returnedProduct != undefined) {
-                    response.write(JSON.stringify(returnedProduct, null, 4));
-                    console.log("Returned product:", returnedProduct.productNumber);
-                    console.log("Operation:", query.operation, "product, dispatching bot.");
-                    // send signal to dispatch bot here
-                } else {
-                    // send back 400 code and log error if sku is not found
-                    response.statusCode = 400;
-                    response.write(JSON.stringify({ error: '400 Sku not found' }));
-                    console.log("Error 400: Sku not found");
-                }
+                fs.readFile('./users.json', 'utf8', function (err, users) {
+                    users = JSON.parse(users);
+                    // verify user based on username and key
+                    if (verifyUser(users, query.user, query.key)) {
+                        var returnedProduct = product.getProduct(query.sku);
+                        // look for sku in database, return object if sku is matching
+                        if (returnedProduct != undefined) {
+                            response.write(JSON.stringify(returnedProduct, null, 4));
+                            console.log("Returned product:", returnedProduct.productNumber);
+                            console.log("Operation:", query.operation, "product, dispatching bot.");
+                            // send signal to dispatch bot here
+                        } else {
+                            // send back 400 code and log error if sku is not found
+                            response.statusCode = 400;
+                            response.write(JSON.stringify({ error: '400 Sku not found' }));
+                            console.log("Error 400: Sku not found");
+                        }
+                    } else {
+                        // send back 4-- code and log error if user is not authenticated
+                        response.write(JSON.stringify({ error: '4-- User not verified.' }));
+                        console.log("Error 4-- User not verified.");
+                    }
+                    response.end();
+                });
             } else if (query.operation == "signup") {
                 fs.readFile('./users.json', 'utf8', function (err, users) {
                     users = JSON.parse(users);
@@ -51,22 +63,37 @@ try {
                         console.log("User", query.user, "signed up for new key.");
                         response.write(JSON.stringify({ key: `${key}` }));
                     } else {
+                        // send back 4-- code and log error if a user with same username is already signed up for key
                         response.write(JSON.stringify({ error: '4-- user already exists.' }));
                         console.log("Error 4-- user already exists.");
                     }
+                    response.end();
                 });
             }
         }
-        response.end();
+        console.log("-----");
     }).listen(config.webAppAccessPort);
 } catch (e) {
     console.log("Error", e);
 }
 
+// check for matching username
 function checkForUser(users, username) {
     for (var i = 0; i < users.length; i++) {
         if (users[i].user == username) {
             return true;
+        }
+    }
+    return false;
+}
+
+// check for matching username and key
+function verifyUser(users, username, key) {
+    for (var i = 0; i < users.length; i++) {
+        if (users[i].user == username) {
+            if (users[i].key == key) {
+                return true;
+            }
         }
     }
     return false;
